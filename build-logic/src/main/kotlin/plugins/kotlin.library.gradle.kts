8b27b1dfb6697d63ec0c0d0a54c7c0b3451c0d7a
KimHyungSik <colrog968@gmail.com>
2023-11-27 23:51:00 +0900

    hilt kotlin module

--- build-logic/src/main/kotlin/plugins/kotlin.library.gradle.kts
+++ build-logic/src/main/kotlin/plugins/kotlin.library.gradle.kts
@@ -1,3 +1,7 @@
 import config.configureHiltKotlin
 
+plugins{
+    kotlin("jvm")
+}
+
 configureHiltKotlin()
--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java-library")
+    id("kotlin.library")
     alias(libs.plugins.kotlin.jvm)
 }
 
