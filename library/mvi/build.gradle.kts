f88890e7bf71b2818912861ddb4b55a4ae3cd578
KimHyungSik <colrog968@gmail.com>
2023-11-30 23:37:51 +0900

    library mvi

--- library/mvi/build.gradle.kts
+++ library/mvi/build.gradle.kts
@@ -3,5 +3,5 @@ plugins {
 }
 
 android {
-    namespace = "com.example.library"
+    namespace = "com.example.mvi"
 }
--- library/mvi/src/test/java/com/example/mvi/ExampleUnitTest.kt
+++ library/mvi/src/test/java/com/example/mvi/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.library
+package com.example.mvi
 
 import org.junit.Test
 
