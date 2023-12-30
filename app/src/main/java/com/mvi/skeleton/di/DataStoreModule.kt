2ba6a181c6b278cd3935cafbbbe63c84b8d3ddff
KimHyungSik <colrog968@gmail.com>
2023-12-30 10:08:07 +0900

    DataStoreItem Module Singleton

--- app/src/main/java/com/mvi/skeleton/di/DataStoreModule.kt
+++ app/src/main/java/com/mvi/skeleton/di/DataStoreModule.kt
@@ -7,10 +7,13 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object DataStoreModule {
+
+    @Singleton
     @Provides
     fun providerDatStore(
         @ApplicationContext appContext: Context
