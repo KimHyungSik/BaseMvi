a3e39a37dd6ad4e3f23e01bb94f13a493b3d5298
KimHyungSik <colrog968@gmail.com>
2023-12-30 10:08:38 +0900

    DataStoreItem Module Singleton

--- data/src/main/java/com/mvi/data/repository/example/ExampleRepositoryImp.kt
+++ data/src/main/java/com/mvi/data/repository/example/ExampleRepositoryImp.kt
@@ -1,14 +1,11 @@
 package com.mvi.data.repository.example
 
-import android.util.Log
 import com.example.pref.datastore.DataStoreItem
 import com.mvi.data.remote.example.ExampleService
 import com.mvi.domain.model.example.ExampleDataDto
 import com.mvi.data.model.example.toDto
-import com.mvi.domain.model.example.SupportDto
 import com.mvi.domain.repository.ExampleRepository
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOf
 import javax.inject.Inject
 
@@ -16,20 +13,11 @@ class ExampleRepositoryImp @Inject constructor(
     private val dataStoreItem: DataStoreItem,
     private val api: ExampleService
 ) : ExampleRepository {
-    override suspend fun getExampleData(): Flow<ExampleDataDto>{
-        Log.d("LOGEE", "ExampleRepositoryImp")
+    override suspend fun getExampleData(): Flow<ExampleDataDto> {
         val exampleResponse = api.getExample()
-        Log.d("LOGEE", "getExampleData: $exampleResponse")
         val dto = exampleResponse.toDto()
-        Log.d("LOGEE", "getExampleData: $exampleResponse")
-        dataStoreItem.put("Key", exampleResponse)
-        val getFromDataStore = dataStoreItem.get(
-            "Key", ExampleDataDto(
-                emptyList(),
-                1, 2, SupportDto("text", "url"), 3, 4
-            )
-        )
-        Log.d("LOGEE", "getExampleData: $getFromDataStore")
+        dataStoreItem.put("Key", dto)
+
         return flowOf(dto)
     }
 }
