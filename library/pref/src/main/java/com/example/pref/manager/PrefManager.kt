455658e45408ef97c7a9f5e92973bb3a0e640112
KimHyungSik <colrog968@gmail.com>
2024-01-07 22:58:16 +0900

    PrefManager

--- library/pref/src/main/java/com/example/pref/manager/PrefManager.kt
+++ library/pref/src/main/java/com/example/pref/manager/PrefManager.kt
@@ -3,48 +3,44 @@ package com.example.pref.manager
 import android.content.Context
 import com.example.pref.Pref
 import com.example.pref.PrefItem
-import com.example.pref.datastore.DataStore
+import com.example.pref.datastore.DataStorePref
 import com.example.pref.memory.MemoryPref
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.flowOf
+import kotlin.reflect.KClass
 
 open class PrefManager(
     private val appContext: Context,
     private val domainName: String
 ) {
-    private val dataStorePref: Pref = DataStore(appContext, domainName)
+    private val dataStorePref: Pref = DataStorePref(appContext, domainName)
     private val memoryPref: Pref = MemoryPref()
-    suspend fun <T> PrefItem<T>.putData(data: T) {
-        getPrefItem(prefStrategy).forEach {
-            it.put(key, data)
-        }
-    }
 
-    suspend fun <T> PrefItem<T>.getData(): Flow<T> {
-        val memoryResult = memoryPref.get(key, defaultValue).first()
-        return if (memoryResult == defaultValue) {
-            dataStorePref.get(key, defaultValue)
-        } else {
-            flowOf(memoryResult)
-        }
-    }
+    fun <T> createPrefItem(key: String, defaultValue: T, type: KClass<*>) =
+        PrefItem(
+            key = key,
+            defaultValue = defaultValue,
+            prefStrategy = PrefStrategy.BOTH_MEMORY_AND_FILE,
+            dataStorePref = dataStorePref,
+            memoryPref = memoryPref,
+            type = type,
+        )
 
-    suspend fun clear(key: String, strategy: PrefStrategy) {
-        getPrefItem(strategy).forEach {
-            it.clear(key)
-        }
-    }
+    fun <T> createMemoryPrefItem(key: String, defaultValue: T, type: KClass<*>) =
+        PrefItem(
+            key = key,
+            defaultValue = defaultValue,
+            prefStrategy = PrefStrategy.MEMORY_ONLY,
+            dataStorePref = dataStorePref,
+            memoryPref = memoryPref,
+            type = type,
+        )
 
-    suspend fun clearAll(key: String, strategy: PrefStrategy) {
-        getPrefItem(strategy).forEach {
-            it.clearAll()
-        }
-    }
-
-    private fun getPrefItem(strategy: PrefStrategy) = when (strategy) {
-        PrefStrategy.MEMORY_ONLY -> listOf(memoryPref)
-        PrefStrategy.FILE_ONLY -> listOf(dataStorePref)
-        PrefStrategy.BOTH_MEMORY_AND_FILE -> listOf(memoryPref, dataStorePref)
-    }
+    fun <T> createPreferencePrefItem(key: String, defaultValue: T, type: KClass<*>) =
+        PrefItem(
+            key = key,
+            defaultValue = defaultValue,
+            prefStrategy = PrefStrategy.FILE_ONLY,
+            dataStorePref = dataStorePref,
+            memoryPref = memoryPref,
+            type = type,
+        )
 }
